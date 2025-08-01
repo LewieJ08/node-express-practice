3b0b30588d9448929441e8512de2bb41f0ed27b2
LewieJ08 <lewiej08@gmail.com>
2025-08-01 15:27:36 +0100

    feat: add users route

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const app = express();
 app.set("view engine", "ejs")
 
 app.get("/", (req, res) => {
-    res.render("index")
+    res.render("index", {text: "world"})
 });  
+
+app.get("/users", (req, res) => {
+    res.send("hello world")
+});
 
 app.listen(3000, () => {
     console.log("Server running on port: 3000");
