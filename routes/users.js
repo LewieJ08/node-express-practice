cbbc85fbbace60c924975bb446c325f3a250294c
LewieJ08 <lewiej08@gmail.com>
2025-08-05 02:55:26 +0100

    feat: clean /users/:id endpoint with route() func

--- routes/users.js
+++ routes/users.js
@@ -15,8 +15,15 @@ router.post("/", (req, res) => {
     res.send("Create User")
 });
 
-router.get("/:id", (req, res) => {
+router.route("/:id")
+    .get((req, res) => {
     res.send(`Get User With ID ${req.params.id}`)
-})
+    })
+    .put((req, res) => {
+    res.send(`Update User With ID ${req.params.id}`)
+    })
+    .delete((req, res) => {
+    res.send(`Delete User With ID ${req.params.id}`)
+    });
 
 module.exports = router;
