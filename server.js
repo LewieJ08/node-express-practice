9cd90bc425df62943a06ea738c58c3b3f6507ccc
LewieJ08 <lewiej08@gmail.com>
2025-08-05 23:37:41 +0100

    feat serve static files in public folder

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require("express");
-
 const app = express();
-app.set("view engine", "ejs")
 
-app.get("/", (req, res) => {
-    res.render("index", {text: "World"})
-});  
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
 
 const userRouter = require("./routes/users");
 
