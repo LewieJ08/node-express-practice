cd14dc71e65befcca2072eafbb06f3305eb3d97f
LewieJ08 <lewiej08@gmail.com>
2025-08-05 03:22:47 +0100

    feat: add params middleware user ID logic

--- routes/users.js
+++ routes/users.js
@@ -17,13 +17,20 @@ router.post("/", (req, res) => {
 
 router.route("/:id")
     .get((req, res) => {
-    res.send(`Get User With ID ${req.params.id}`)
+        console.log(req.user);
+        res.send(`Get User With ID ${req.params.id}`);
     })
     .put((req, res) => {
-    res.send(`Update User With ID ${req.params.id}`)
+        res.send(`Update User With ID ${req.params.id}`);
     })
     .delete((req, res) => {
-    res.send(`Delete User With ID ${req.params.id}`)
+        res.send(`Delete User With ID ${req.params.id}`);
     });
+
+const users = [{name: "kyle"}, {name: "sally"}];
+router.param("id",(req, res, next, id) => {
+    req.user = users[id];
+    next();
+});
 
 module.exports = router;
